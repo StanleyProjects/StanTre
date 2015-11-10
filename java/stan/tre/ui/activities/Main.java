30b51477d7f1c0eb8cd967a304462a33370f2aa3
antonoctopod <a.grechushnikov@octopod.com>
2015-11-10 15:48:12 +0200

    init

---
+++ java/stan/tre/ui/activities/Main.java
@@ -0,0 +1,17 @@
+package stan.tre.ui.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import stan.tre.R;
+
+public class Main
+    extends Activity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
