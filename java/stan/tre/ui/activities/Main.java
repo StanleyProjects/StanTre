a16a74574a263d10589b9c5d51710393af6b6caf
antonoctopod <a.grechushnikov@octopod.com>
2015-11-10 18:06:05 +0200

    test lib

--- java/stan/tre/ui/activities/Main.java
+++ java/stan/tre/ui/activities/Main.java
@@ -1,17 +1,81 @@
 package stan.tre.ui.activities;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.util.Log;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import stan.tre.R;
 
 public class Main
-    extends Activity
+        extends StanActivity
 {
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
+
+    TessBaseAPI baseApi;
+
+    public Main()
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        super(R.layout.main, R.id.main_frame);
+    }
+
+    @Override
+    protected void initFragments()
+    {
+
+    }
+
+    @Override
+    protected void initViews()
+    {
+
+    }
+
+    @Override
+    protected void init()
+    {
+        initTess();
+    }
+
+    private void initTess()
+    {
+        baseApi = new TessBaseAPI();
+        String filepath = "tessdata";
+        String filename = "rus.traineddata";
+        String absolutePath = this.getFilesDir().getAbsolutePath();
+        File file = new File(absolutePath, filepath + "/" + filename);
+        if(!file.exists())
+        {
+            try
+            {
+                InputStream in = this.getApplicationContext().getResources().getAssets().open(filepath + "/" + filename);
+                file = new File(this.getFilesDir(), filepath);
+                file.mkdir();
+                file.mkdirs();
+                file = new File(this.getFilesDir(), filepath + "/" + filename);
+                file.createNewFile();
+                OutputStream out = new FileOutputStream(file);
+                byte[] buf = new byte[1024];
+                int len;
+                while((len = in.read(buf)) > 0)
+                {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+                in = null;
+                out = null;
+            }
+            catch(IOException e)
+            {
+                Log.e("tess", e.getMessage());
+            }
+        }
+        baseApi.init(absolutePath, "rus");
     }
 }
